b353dbfe9c3248c1d23d3a724fe1bd3ad94b701b
Mihael Koep <mihael.koep@softwareschneiderei.de>
2022-02-03 09:12:19 +0100

    Fresh micronaut launch app with tomcat support.

---
+++ src/test/groovy/de/miq/demo/DemoTomcatSpec.groovy
@@ -0,0 +1,19 @@
+package de.miq.demo
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class DemoTomcatSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
